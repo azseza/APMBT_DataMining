3ca44a30def03b06db75b2c0fa75c3a57781579f
azseza <you@example.com>
2020-11-21 19:14:05 +0100

    Data sources loaded for scarping /DataInit/sources.txt ~~

--- DataCollecters/doingit.py
+++ DataCollecters/doingit.py
@@ -8,15 +8,14 @@ import csv
 import urllib.request
 print("opening CSV file")
 #opening links.csv to extract the good links
-i=0
+i=1
 with open('links.csv','r') as csvfile:
     reader = csv.DictReader(csvfile)
     print("done opening the file")
     for row in reader :
         if row['Is it Downloadble'] == 'True' :
-            print("downloading file  "+str(i)+" : "+row['Dirty link'])
+            print("downloading file number  "+str(i)+" : "+row['Dirty link'])
             #urllib.request.urlretrieve(row['Clean link'])
             urllib.request.urlretrieve(row['Clean link'])
             print("Done !! ")
             i+=1
-#extracting them in the same directory
